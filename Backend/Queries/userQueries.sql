2c322b8d62ebc1fad2fb3d1d5fab41ac2747707d
Chris-G32 <cgruska@kent.edu>
2023-04-25 09:48:34 -0400

    added public userview query

---
+++ Backend/Queries/userQueries.sql
@@ -0,0 +1,10 @@
+--Start Views Section
+/* Create view for public user information */
+CREATE VIEW PublicUserInfo AS
+SELECT User_ID,Display_Name,Bio
+FROM userprofile;
+
+--End Views Section
+--Start Functions Section
+
+--End Functions Section
